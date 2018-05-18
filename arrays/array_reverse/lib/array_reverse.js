729042404ede4bb039ac68987000ce6a626addb0
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-05-17 20:35:04 -0700

    wokring on testing

--- arrays/array_reverse/lib/array_reverse.js
+++ arrays/array_reverse/lib/array_reverse.js
@@ -2,7 +2,7 @@
 function reverseArray(input) {
   var answer = new Array;
   for(var i = input.length-1; i >= 0; i--) {
-      answer.push(input[i]);
+    answer.push(input[i]);
   }
   return answer;
 }
