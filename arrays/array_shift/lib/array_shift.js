cc7089d9695174be0d8d270ac023a1d381bf1bce
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-05-19 15:47:44 -0700

    travis

--- arrays/array_shift/lib/array_shift.js
+++ arrays/array_shift/lib/array_shift.js
@@ -4,7 +4,7 @@ module.exports = exports = {};
 
 exports.insertShiftArray = (arr, val) => {
   let newArr = [];
-  let middle = arr.length/2;
+  let middle = Math.ceil(arr.length/2);
 
   for(let i = 0; i < arr.length +1; i++) {
     if(i < middle) {
--- arrays/array_shift/test/array_shift.spec.js
+++ arrays/array_shift/test/array_shift.spec.js
@@ -7,7 +7,7 @@ describe('Array Shift', () => {
   it('shiftArray() will take an input array and will take the second input and place it in the middle of the array', () => {
     expect(arrayShift.insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
   });
-  // it('shiftArray() will take an input array and will take the second input and place it in the middle of the array', () => {
-  //   expect(arrayShift.insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
-  // });
+  it('shiftArray() will take an input array and will take the second input and place it in the middle of the array', () => {
+    expect(arrayShift.insertShiftArray([4, 8, 15, 23, 42], 16)).toEqual([4, 8, 15, 16, 23, 42]);
+  });
 });
